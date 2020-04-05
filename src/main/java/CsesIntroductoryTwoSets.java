d7f51c3c711fbd5792f89d4f5d10f17690d520e9
Efim Pyshnograev <efim@fastmail.com>
2020-04-05 13:16:27 +1000

    Solve bit sets

--- src/main/java/CsesIntroductoryTwoSets.java
+++ src/main/java/CsesIntroductoryTwoSets.java
@@ -1,3 +1,6 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class CsesIntroductoryTwoSets {
 
     public static void main(String[] args) {
@@ -5,7 +8,44 @@ public class CsesIntroductoryTwoSets {
     }
 
     public void solve() {
+        final Scanner in = new Scanner(System.in);
+        final PrintWriter out = new PrintWriter(System.out);
 
+        int n = in.nextInt();
+
+        if (n % 4 != 0 && (n + 1) % 4 != 0) {
+            out.println("NO");
+            out.flush();
+            return;
+        }
+
+        out.println("YES");
+        if (n % 4 == 0) {
+            out.println(n / 2);
+            for (int i = 1; i <= n / 4; i++) {
+                out.print(i + " " + (n - i + 1) + " ");
+            }
+            out.println();
+            out.println(n / 2);
+            for (int i = n / 4 + 1; i <= n / 2; i++) {
+                out.print(i + " " + (n - i + 1) + " ");
+            }
+            out.println();
+        } else {
+            n += 1;
+            out.println(n / 2);
+            out.print("1 " + n / 2 + " ");
+            for (int i = 2; i <= n / 4; i++) {
+                out.print(i + " " + (n - i + 1) + " ");
+            }
+            out.println();
+            out.println(n / 2 - 1);
+            for (int i = n / 4 + 1; i < n / 2; i++) {
+                out.print(i + " " + (n - i + 1) + " ");
+            }
+            out.println(n / 2 + 1);
+        }
+        out.flush();
     }
 
 }
--- src/test/java/CsesIntroductoryTwoSetsTest.java
+++ src/test/java/CsesIntroductoryTwoSetsTest.java
@@ -9,11 +9,26 @@ class CsesIntroductoryTwoSetsTest extends AcmTest {
 
     @Test
     public void sample1() throws Exception {
-        compare("7", "YES\n4\n1 2 4 7\n3\n3 5 6");
+        compare("7", "YES\n4\n1 4 2 7 \n3\n3 6 5");
     }
 
     @Test
     public void sample2() throws Exception {
+        compare("8", "YES\n4\n1 8 2 7 \n4\n3 6 4 5");
+    }
+
+    @Test
+    public void sample3() throws Exception {
         compare("6", "NO");
+    }
+
+    @Test
+    public void trivial1() throws Exception {
+        compare("3", "YES\n2\n1 2 \n1\n3");
+    }
+
+    @Test
+    public void trivial2() throws Exception {
+        compare("4", "YES\n2\n1 4 \n2\n2 3");
     }
 }
