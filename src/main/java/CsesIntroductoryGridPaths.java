333bc184b682ff30e6a5c98eff6c426fcfb6e35f
Efim Pyshnograev <efim@fastmail.com>
2020-04-10 09:47:17 +1000

    Replace boolean array with long mask

--- src/main/java/CsesIntroductoryGridPaths.java
+++ src/main/java/CsesIntroductoryGridPaths.java
@@ -12,12 +12,12 @@ public class CsesIntroductoryGridPaths {
     public void solve() throws Exception {
         final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         final char[] pattern = in.readLine().toCharArray();
-        final boolean[][] board = new boolean[n][n];
+        final long board = 0L;
 
         System.out.println(ways(pattern, 0, 0, 0, board));
     }
 
-    private long ways(char[] pattern, int position, int I, int J, boolean[][] board) {
+    private long ways(char[] pattern, int position, int I, int J, long board) {
         if (I == n - 1 && J == 0 && position < pattern.length) {
             return 0;
         }
@@ -27,43 +27,34 @@ public class CsesIntroductoryGridPaths {
                 return 0;
             }
 
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board.length; j++) {
-                    if (i == n - 1 && j == 0) {
-                        continue;
-                    }
-                    if (!board[i][j]) {
-                        return 0;
-                    }
-                }
+            if (board != ((1L << (n * n)) - 1) - (1L << (n * (n - 1)))) {
+                return 0;
             }
 
             return 1;
         }
 
-        if (I < 0 || I >= n || J < 0 || J >= n || board[I][J]) {
+        if (I < 0 || I >= n || J < 0 || J >= n || (board & (1L << (I * n + J))) != 0) {
             return 0;
         }
 
-        board[I][J] = true;
-
+        long newBoard = board | (1L << (I * n + J));
 
         long result = 0;
         char command = pattern[position];
         if (command == 'U' || command == '?') {
-            result += ways(pattern, position + 1, I - 1, J, board);
+            result += ways(pattern, position + 1, I - 1, J, newBoard);
         }
         if (command == 'D' || command == '?') {
-            result += ways(pattern, position + 1, I + 1, J, board);
+            result += ways(pattern, position + 1, I + 1, J, newBoard);
         }
         if (command == 'L' || command == '?') {
-            result += ways(pattern, position + 1, I, J - 1, board);
+            result += ways(pattern, position + 1, I, J - 1, newBoard);
         }
         if (command == 'R' || command == '?') {
-            result += ways(pattern, position + 1, I, J + 1, board);
+            result += ways(pattern, position + 1, I, J + 1, newBoard);
         }
 
-        board[I][J] = false;
         return result;
     }
 
