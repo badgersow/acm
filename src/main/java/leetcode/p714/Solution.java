06288a84987c6ebdea1c2b70e62442611019864d
Efim Pyshnograev <efim@fastmail.com>
2020-04-26 15:45:47 +1000

    Solve Leetcode "Best time to Buy and Sell Stock"

--- src/main/java/leetcode/p714/Solution.java
+++ src/main/java/leetcode/p714/Solution.java
@@ -2,6 +2,17 @@ package leetcode.p714;
 
 public class Solution {
     public int maxProfit(int[] prices, int fee) {
-        return 0;
+        final int n = prices.length;
+        int optimal = 0;
+        int maxOptMinusPrice = -prices[0] - fee;
+        for (int i = 1; i < n; i++) {
+            // If I don't sell here, my result is like previous.
+            // I will just try to improve it by selling at current point
+            final int ifISellNow = prices[i] + maxOptMinusPrice;
+            optimal = Math.max(optimal, ifISellNow);
+            maxOptMinusPrice = Math.max(maxOptMinusPrice, optimal - prices[i] - fee);
+        }
+
+        return optimal;
     }
 }
--- src/test/java/leetcode/p714/P714SolutionTest.java
+++ src/test/java/leetcode/p714/P714SolutionTest.java
@@ -13,7 +13,8 @@ public class P714SolutionTest {
     @CsvSource(value = {
             "1 3 2 8 4 9, 2, 8",
             "5 4 3 2 1, 10, 0",
-            "0 10 0 11, 1, 19"
+            "0 10 0 11, 1, 19",
+            "0, 0, 0"
     })
     public void checkSolution(String array, int fee, long expectedResult) {
         final int[] prices = Arrays.stream(array.split(" "))
