8b5aa2abbda894551885866f9905833c00aa50cd
Efim Pyshnograev <epyshnograev@atlassian.com>
2020-05-01 16:17:19 +1000

    Solve "NextNode"

--- src/main/java/itya/NextNode.java
+++ src/main/java/itya/NextNode.java
@@ -3,7 +3,52 @@ package itya;
 public class NextNode {
 
     public static void connect(Node v) {
+        assign(v, null);
+    }
 
+    static void assign(Node v, Node parent) {
+        if (v.next != null) {
+            return;
+        }
+
+        Node parentOfNext = null;
+        if (parent == null) {
+            v.next = null;
+            parentOfNext = null;
+        } else {
+            if (parent.left == v && parent.right != null) {
+                v.next = parent.right;
+                parentOfNext = parent;
+            } else {
+                Node nextParent = parent.next;
+                while (nextParent != null) {
+                    Node u = leftmost(nextParent);
+                    if (u != null) {
+                        v.next = u;
+                        parentOfNext = nextParent;
+                        break;
+                    }
+                    nextParent = nextParent.next;
+                }
+            }
+        }
+
+        if (v.next != null) {
+            assign(v.next, parentOfNext);
+        }
+        if (v.left != null) {
+            assign(v.left, v);
+        }
+        if (v.right != null) {
+            assign(v.right, v);
+        }
+    }
+
+    static Node leftmost(Node v) {
+        if (v.left != null) {
+            return v.left;
+        }
+        return v.right;
     }
 
     public static class Node {
