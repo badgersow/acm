09972339bb43f3ec7391f7f87d611b8ff7e524e2
Efim Pyshnograev <epyshnograev@atlassian.com>
2020-08-26 10:00:59 +1000

    Solve String Removals

---
+++ src/test/java/CsesAdditionalStringRemovalsTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+public class CsesAdditionalStringRemovalsTest extends AcmTest {
+
+    @Override
+    void processInput() throws Exception {
+        new CsesAdditionalStringRemovals().solve();
+    }
+
+    @Test
+    public void sample() {
+        compare("aybabtu", "103");
+    }
+
+    @Test
+    public void trivial1() {
+        compare("aa", "2");
+    }
+
+    @Test
+    public void runtimeError1() {
+        compare(readFile("/cses_additional_string_reordering_re_in_1.txt"),
+                readFile("/cses_additional_string_reordering_re_out_1.txt"));
+    }
+
+}
