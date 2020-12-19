ea05c016a23a36df9384ea5a95a2d9f830a99e99
Efim Pyshnograev <epyshnograev@atlassian.com>
2020-12-19 16:21:50 +1100

    Start solving springboards

---
+++ src/main/java/usaco/y2020/gold/Springboards.java
@@ -0,0 +1,192 @@
+package usaco.y2020.gold;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Springboards {
+
+    public static void main(String[] args) throws Exception {
+        new Springboards().solve();
+    }
+
+    private final FastReader in = new FastReader();
+
+    private final PrintWriter out = new PrintWriter(System.out);
+
+    final int MAX_SPRINGBOARDS = 100_001;
+
+    public void solve() throws Exception {
+        final int n = in.nextInt();
+        final int P = in.nextInt();
+
+        int[][] boards = new int[n][P];
+
+        final Map<Integer, Integer> compressedByReal = new HashMap<>();
+        final int[] realByCompressed = new int[MAX_SPRINGBOARDS];
+        final SortedSet<Integer> coordinates = new TreeSet<>();
+
+        for (int i = 0; i < P; i++) {
+            final int startX = in.nextInt();
+            final int startY = in.nextInt();
+            final int endX = in.nextInt();
+            final int endY = in.nextInt();
+
+            boards[i] = new int[]{startX, startY, endX, endY};
+            coordinates.addAll(Arrays.asList(startX, startY, endX, endY));
+        }
+
+        int index = 0;
+        for (Integer coordinate : coordinates) {
+            realByCompressed[index] = coordinate;
+            compressedByReal.put(coordinate, index);
+            index++;
+        }
+
+
+        out.flush();
+    }
+
+    private static class FastReader {
+
+        private final int BUFFER_SIZE = 1 << 24;
+        private final DataInputStream din;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public FastReader(String fileName) {
+            try {
+                din = new DataInputStream(new FileInputStream(fileName));
+                buffer = new byte[BUFFER_SIZE];
+                bufferPointer = bytesRead = 0;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public String readLine() {
+            byte[] buf = new byte[1001]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() {
+            try {
+                bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+                if (bytesRead == -1)
+                    buffer[0] = -1;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private byte read() {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() {
+            try {
+                din.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public int[] fillIntegerArray(int n) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+                array[i] = nextInt();
+            return array;
+        }
+
+        public long[] fillLongArray(int n) {
+            long[] array = new long[n];
+            for (int i = 0; i < n; i++)
+                array[i] = nextLong();
+            return array;
+        }
+
+        public <T extends List<Long>> T fillList(T list, int n) {
+            for (int i = 0; i < n; i++)
+                list.add(nextLong());
+            return list;
+        }
+    }
+}
---
+++ src/test/java/usaco/y2020/gold/SpringboardsTest.java
@@ -0,0 +1,108 @@
+package usaco.y2020.gold;
+
+import org.junit.jupiter.api.Test;
+import util.AcmTest;
+
+public class SpringboardsTest extends AcmTest {
+
+    @Override
+    public void processInput() throws Exception {
+        new Springboards().solve();
+    }
+
+    @Test
+    public void sample() {
+        compare("3 2 0 1 0 2 1 2 2 3", "3");
+    }
+
+    @Test
+    public void officialTest1() {
+        compare(readFile("/usaco/2020/gold/springboards/1.in"),
+                readFile("/usaco/2020/gold/springboards/1.out"));
+    }
+
+    @Test
+    public void officialTest2() {
+        compare(readFile("/usaco/2020/gold/springboards/2.in"),
+                readFile("/usaco/2020/gold/springboards/2.out"));
+    }
+
+    @Test
+    public void officialTest3() {
+        compare(readFile("/usaco/2020/gold/springboards/3.in"),
+                readFile("/usaco/2020/gold/springboards/3.out"));
+    }
+
+    @Test
+    public void officialTest4() {
+        compare(readFile("/usaco/2020/gold/springboards/4.in"),
+                readFile("/usaco/2020/gold/springboards/4.out"));
+    }
+
+    @Test
+    public void officialTest5() {
+        compare(readFile("/usaco/2020/gold/springboards/5.in"),
+                readFile("/usaco/2020/gold/springboards/5.out"));
+    }
+
+    @Test
+    public void officialTest6() {
+        compare(readFile("/usaco/2020/gold/springboards/6.in"),
+                readFile("/usaco/2020/gold/springboards/6.out"));
+    }
+
+    @Test
+    public void officialTest7() {
+        compare(readFile("/usaco/2020/gold/springboards/7.in"),
+                readFile("/usaco/2020/gold/springboards/7.out"));
+    }
+
+    @Test
+    public void officialTest8() {
+        compare(readFile("/usaco/2020/gold/springboards/8.in"),
+                readFile("/usaco/2020/gold/springboards/8.out"));
+    }
+
+    @Test
+    public void officialTest9() {
+        compare(readFile("/usaco/2020/gold/springboards/9.in"),
+                readFile("/usaco/2020/gold/springboards/9.out"));
+    }
+
+    @Test
+    public void officialTest10() {
+        compare(readFile("/usaco/2020/gold/springboards/10.in"),
+                readFile("/usaco/2020/gold/springboards/10.out"));
+    }
+
+    @Test
+    public void officialTest11() {
+        compare(readFile("/usaco/2020/gold/springboards/11.in"),
+                readFile("/usaco/2020/gold/springboards/11.out"));
+    }
+
+    @Test
+    public void officialTest12() {
+        compare(readFile("/usaco/2020/gold/springboards/12.in"),
+                readFile("/usaco/2020/gold/springboards/12.out"));
+    }
+
+    @Test
+    public void officialTest13() {
+        compare(readFile("/usaco/2020/gold/springboards/13.in"),
+                readFile("/usaco/2020/gold/springboards/13.out"));
+    }
+
+    @Test
+    public void officialTest14() {
+        compare(readFile("/usaco/2020/gold/springboards/14.in"),
+                readFile("/usaco/2020/gold/springboards/14.out"));
+    }
+
+    @Test
+    public void officialTest15() {
+        compare(readFile("/usaco/2020/gold/springboards/15.in"),
+                readFile("/usaco/2020/gold/springboards/15.out"));
+    }
+
+}
