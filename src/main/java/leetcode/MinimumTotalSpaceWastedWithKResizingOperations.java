4f9245969f0a75635d63f43493383ddde67dde9a
Efim Pyshnograev <epyshnograev@atlassian.com>
2021-08-15 11:25:01 +1000

    Solve the problem using the DP

--- src/main/java/leetcode/MinimumTotalSpaceWastedWithKResizingOperations.java
+++ src/main/java/leetcode/MinimumTotalSpaceWastedWithKResizingOperations.java
@@ -11,55 +11,46 @@ public class MinimumTotalSpaceWastedWithKResizingOperations {
 
         this.n = a.length;
         this.a = a;
-        this.dp = new int[n][k + 1][n];
-        for (int[][] ints : dp) {
-            for (int[] ints2 : ints) {
-                Arrays.fill(ints2, -1);
-            }
+        this.dp = new int[n][k + 2];
+        for (int[] layer : dp) {
+            Arrays.fill(layer, -1);
         }
 
-        return waste(1, k, 0);
+        return waste(0, k + 1);
     }
-
-    private int[][][] dp;
 
     private int[] a;
 
     private int n;
 
-    private int waste(int index, int changes, int prevIndex) {
+    private int[][] dp;
+
+    // Waste of the interval [index..n) assuming k intervals left
+    private int waste(int index, int intervals) {
         if (index == n) {
             return 0;
         }
-
-        if (dp[index][changes][prevIndex] >= 0) {
-            return dp[index][changes][prevIndex];
+        if (intervals == 0) {
+            return Integer.MAX_VALUE / 2;
+        }
+        if (dp[index][intervals] >= 0) {
+            return dp[index][intervals];
         }
 
+        // Now let's create an interval ending in I inclusive
+        int sum = 0;
+        int max = -1;
         int result = Integer.MAX_VALUE;
+        for (int i = index; i < n; i++) {
+            max = Math.max(max, a[i]);
+            sum += a[i];
 
-        // If we have K, we can always resize to the current number.
-        // Waste of the current element is going to be 0.
-        if (changes > 0) {
             result = Math.min(result,
-                    waste(index + 1, changes - 1, index));
+                    (i - index + 1) * max - sum + // waste of the current interval
+                            waste(i + 1, intervals - 1));
         }
 
-        // We can also resize from the previous number, but ONLY if
-        // the previous number is <= the current.
-        if (a[prevIndex] <= a[index]) {
-            result = Math.min(result,
-                    (index - prevIndex) * (a[index] - a[prevIndex]) +
-                            waste(index + 1, changes, index));
-        }
-
-        // Finally, we can just continue, but only if the previous number is OK
-        if (a[index] <= a[prevIndex]) {
-            result = Math.min(result,
-                    (a[prevIndex] - a[index]) +
-                            waste(index + 1, changes, prevIndex));
-        }
-
-        return (dp[index][changes][prevIndex] = result);
+        return (dp[index][intervals] = result);
     }
+
 }
