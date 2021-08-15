41c1e01de86c37d212948fcb0a5833a4df69e204
Efim Pyshnograev <epyshnograev@atlassian.com>
2021-08-15 13:15:32 +1000

    Solve the last problem

--- src/main/java/leetcode/contest/weekly254/LastDayWhereYouCanStillCross.java
+++ src/main/java/leetcode/contest/weekly254/LastDayWhereYouCanStillCross.java
@@ -1,11 +1,16 @@
 package leetcode.contest.weekly254;
 
+import java.util.Arrays;
+
 public class LastDayWhereYouCanStillCross {
 
     public int latestDayToCross(int row, int col, int[][] cells) {
         this.a = new int[row][col];
+        this.visited = new boolean[row][col];
+        this.I = row;
+        this.J = col;
         for (int i = 0; i < cells.length; i++) {
-            a[cells[i][0]][cells[i][1]] = i;
+            a[cells[i][0] - 1][cells[i][1] - 1] = i;
         }
 
         // Let's start the binary search
@@ -24,9 +29,51 @@ public class LastDayWhereYouCanStillCross {
     }
 
     private boolean canCross(int day) {
+        for (boolean[] layer : visited) {
+            Arrays.fill(layer, false);
+        }
+
+        for (int j = 0; j < J; j++) {
+            if (a[0][j] >= day && canReachBottom(0, j, day)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean canReachBottom(int i, int j, int day) {
+        if (i == I - 1) {
+            // This is the last row
+            return true;
+        }
+        visited[i][j] = true;
+
+        for (int[] diff : d) {
+            int newI = i + diff[0];
+            int newJ = j + diff[1];
+
+            if (newI < 0 || newI >= I || newJ < 0 || newJ >= J) {
+                continue;
+            }
+
+            if (visited[newI][newJ] || a[newI][newJ] < day) {
+                continue;
+            }
+
+            if (canReachBottom(newI, newJ, day)) {
+                return true;
+            }
+        }
+
         return false;
     }
 
     int[][] a;
 
+    boolean[][] visited;
+
+    int I, J;
+
+    int[][] d = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 }
--- src/test/java/leetcode/contest/weekly254/LastDayWhereYouCanStillCrossTest.java
+++ src/test/java/leetcode/contest/weekly254/LastDayWhereYouCanStillCrossTest.java
@@ -6,6 +6,20 @@ import org.junit.jupiter.api.Test;
 
 class LastDayWhereYouCanStillCrossTest {
     @Test
+    public void trivial0() {
+        Assertions.assertThat(new LastDayWhereYouCanStillCross()
+                .latestDayToCross(
+                        2,
+                        2, new
+                                int[][]{
+                                {1, 1},
+                                {1, 2},
+                                {2, 1},
+                                {2, 2}
+                        })).isEqualTo(1);
+    }
+
+    @Test
     public void sample1() {
         Assertions.assertThat(new LastDayWhereYouCanStillCross()
                 .latestDayToCross(
