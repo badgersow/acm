3541789f3dc9f360e2ec4a1dedd4d1a3f2828599
Efim Pyshnograev <epyshnograev@atlassian.com>
2021-09-02 19:24:39 +1000

    Solve the problem in a google way with O(1) memory

--- src/main/java/interview/google/WaterCalculator.java
+++ src/main/java/interview/google/WaterCalculator.java
@@ -6,7 +6,7 @@ public class WaterCalculator {
      * Calculate total amount of water given the array of heights.
      * Works in O(heights.length) time and consumes additional O(heights.length) memory
      */
-    public int waterVolume(int[] heights) {
+    public int waterVolume1(int[] heights) {
         int n = heights.length;
         // maxSuffix[i] - maximum on [i..n)
         int[] maxSuffix = new int[n + 1];
@@ -32,4 +32,28 @@ public class WaterCalculator {
 
         return totalWater;
     }
+
+    public int waterVolume2(int[] heights) {
+        int n = heights.length;
+        int left = 0, right = n - 1;
+        // Maximum height from leftmost sink to left) and from (right to rightmost sink
+        int leftMax = 0, rightMax = 0;
+        int total = 0;
+        while (left <= right) {
+            // Water in the position “left” will flow to the left, because to the right we know the bar is at least same hight
+            if (leftMax <= rightMax) {
+                total += Math.max(0, leftMax - heights[left]);
+                leftMax = Math.max(leftMax, heights[left]);
+                left++;
+            } else {
+                total += Math.max(0, rightMax - heights[right]);
+                rightMax = Math.max(rightMax, heights[right]);
+                right--;
+            }
+        }
+
+        return total;
+    }
+
+
 }
--- src/test/java/interview/google/WaterCalculatorTest.java
+++ src/test/java/interview/google/WaterCalculatorTest.java
@@ -15,10 +15,24 @@ class WaterCalculatorTest {
             ";0",
             "1;0"
     })
-    void test(String input, int expectedResult) {
-        Assertions.assertThat(new WaterCalculator().waterVolume(
+    void test1(String input, int expectedResult) {
+        Assertions.assertThat(new WaterCalculator().waterVolume1(
                 (input != null ? Stream.of(input.split(",")).mapToInt(Integer::valueOf).toArray() : new int[]{})
         )).isEqualTo(expectedResult);
     }
+
+    @ParameterizedTest
+    @CsvSource(delimiter = ';', value = {
+            "3,2,8,5;1",
+            "1,3,2,4,1,3,1,4,5,2,2,1,4,2,2;15",
+            ";0",
+            "1;0"
+    })
+    void test2(String input, int expectedResult) {
+        Assertions.assertThat(new WaterCalculator().waterVolume2(
+                (input != null ? Stream.of(input.split(",")).mapToInt(Integer::valueOf).toArray() : new int[]{})
+        )).isEqualTo(expectedResult);
+    }
+
 
 }
